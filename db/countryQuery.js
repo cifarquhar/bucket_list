0c3537e9cdf48bb34aba8094bf26906ae0343046
Colin Farquhar <cifarquhar@gmail.com>
2017-04-19 11:53:47 +0100

    add CountryQuery constructor

---
+++ db/countryQuery.js
@@ -0,0 +1,22 @@
+var MongoClient = require("mongodb").MongoClient
+
+var CountryQuery = function(){
+  this.url = "mongodb://localhost:27017/bucket_list"
+}
+
+CountryQuery.prototype = {
+
+  all: function(onQueryFinished){
+    MongoClient.connect(this.url,function(err,db){
+      if(db){
+        var collection = db.collection("countries")
+        collection.find().toArray(function(err,docs){
+          onQueryFinished(docs)
+        })
+      }
+    })
+  }
+
+}
+
+module.exports = CountryQuery
